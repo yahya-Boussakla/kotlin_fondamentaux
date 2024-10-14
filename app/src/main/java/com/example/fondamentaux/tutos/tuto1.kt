3af90afa8df19e90ea7a783d5cd4d23b567ce32d
yahya <yahyaboussakl@gmail.com>
2024-10-14 13:40:28 +0100

    tuto1

---
+++ app/src/main/java/com/example/fondamentaux/tutos/tuto1.kt
@@ -0,0 +1,12 @@
+package com.example.fondamentaux.tutos
+
+fun main() {
+    val x: Any = 4
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
+    }
+}
