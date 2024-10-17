8992259b395a23dd85d038f80a906fe56afdc43f
yahya <yahyaboussakl@gmail.com>
2024-10-17 16:45:08 +0100

    dice

--- app/src/main/java/com/example/fondamentaux/MainActivity.kt
+++ app/src/main/java/com/example/fondamentaux/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.fondamentaux.ui.theme.FondamentauxTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +29,40 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FondamentauxTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.dice_1)
+            , contentDescription = "dado dlhafa"
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {  }) {
+            Text(
+                text = "test"
+            )
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+
+
+@Preview(showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    FondamentauxTheme {
-        Greeting("Android")
-    }
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
 }
--- app/src/main/java/com/example/fondamentaux/tutos/tuto1.kt
+++ app/src/main/java/com/example/fondamentaux/tutos/tuto1.kt
@@ -1,16 +1,29 @@
 package com.example.fondamentaux.tutos
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import kotlin.reflect.KType
+import kotlin.reflect.typeOf
+
+
+
+@RequiresApi(Build.VERSION_CODES.O)
 fun main() {
 
 
-    class Circle(val radius:Double) {
-        constructor(name:String) : this(1.0)
-        constructor(diameter:Int) : this(diameter / 2.0) {
-            println("in diameter constructor")
-        }
-        init {
-            println("Area: ${3 * radius * radius}")
-        }
-    }
-    val c = Circle("yqhyq")
+//    class Circle(val radius:Double) {
+//        constructor(name:String) : this(1.0)
+//        constructor(diameter:Int) : this(diameter / 2.0) {
+//            println("in diameter constructor")
+//        }
+//        init {
+//            println("Area: ${3 * radius * radius}")
+//        }
+//    }
+//    val c = Circle("yqhyq")
+
+    val test =  3.56
+
+    println("degrees $test.")
+
 }
